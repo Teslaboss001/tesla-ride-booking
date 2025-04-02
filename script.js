78a4473dc81f477155f9348fcf5a8654e4eeaba8
Teslaboss001 <khps5406@gmail.com>
2025-04-02 17:43:41 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 
-const bookBtn = document.getElementById('bookBtn');
-const categoryBtns = document.getElementById('categoryBtns');
+document.addEventListener("DOMContentLoaded", () => {
+  const bookBtn = document.getElementById("bookBtn");
+  const categoryBtns = document.getElementById("categoryBtns");
+  const categoryBtnList = document.querySelectorAll(".category-btn");
+  const formContainer = document.getElementById("formContainer");
 
-bookBtn.addEventListener('click', function() {
-  bookBtn.style.display = 'none';
-  categoryBtns.style.display = 'block';
+  bookBtn.addEventListener("click", () => {
+    categoryBtns.classList.remove("hidden");
+  });
+
+  categoryBtnList.forEach(btn => {
+    btn.addEventListener("click", () => {
+      formContainer.classList.remove("hidden");
+    });
+  });
 });
