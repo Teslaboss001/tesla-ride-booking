bda6eaf198cd5310e92b00d531309d6b12cf62c8
Teslaboss001 <khps5406@gmail.com>
2025-04-02 13:50:07 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-const bookBtn = document.getElementById('bookBtn');
-const categoryBtns = document.getElementById('categoryBtns');
-
-bookBtn.addEventListener('click', function() {
-  bookBtn.style.display = 'none';
-  categoryBtns.style.display = 'block';
-});
