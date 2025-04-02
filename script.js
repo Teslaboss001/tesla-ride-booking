a3a50c801d62311124c8014a3e3737ed689f7ef5
Teslaboss001 <khps5406@gmail.com>
2025-04-02 09:39:34 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 
-const bookBtn = document.getElementById('bookBtn');
-const categoryBtns = document.getElementById('categoryBtns');
-
-bookBtn.addEventListener('click', function() {
-  bookBtn.style.display = 'none';
-  categoryBtns.style.display = 'block';
+document.getElementById("startButton").addEventListener("click", () => {
+  const form = document.createElement("div");
+  form.innerHTML = `
+    <p>這裡可以放表單選項</p>
+  `;
+  document.getElementById("formContainer").appendChild(form);
+  document.getElementById("startButton").style.display = "none";
 });
